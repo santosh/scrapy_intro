2ca519972a65eaf9506548956a0d4fa1fdd0cb00
Santosh Kumar <sntshkmr60@gmail.com>
2025-01-26 06:08:33 +0530

    refactor extraction of title, author and tag

--- scrapy_intro/spiders/quote.py
+++ scrapy_intro/spiders/quote.py
@@ -6,26 +6,8 @@ class QuoteSpider(scrapy.Spider):
     start_urls = ["https://quotes.toscrape.com"]
 
     def parse(self, response):
-        # in this example, extract <title> of the page
-        title = response.css('title::text').extract()
-        yield {'titletext': title}
-
-        # extract all quotes
-        quotes = response.css('div.quote>span.text::text').extract()
-
-        yield {'quotes': quotes}
-
-        # extract all authors
-        authors = response.css('div.quote>span small.author::text').extract()
-        yield {'authors': authors}
-
-        # another example using xpath
-        for quote in response.xpath('//div[@class="quote"]'):
-            text = quote.xpath('span[@class="text"]/text()').extract_first()
-            author = quote.xpath('span/small[@class="author"]/text()').extract_first()
-            yield {'text': text, 'author': author}
-
-        # combining css selectors and xpath
-        next_link = response.css("li.next a").xpath("@href").extract_first()
-        
-        yield {'next_link': next_link}
+        all_div_quotes = response.css("div.quote")
+        title = all_div_quotes.css("span.text::text").extract()
+        author = all_div_quotes.css(".author::text").extract()
+        tag = all_div_quotes.css(".tag::text").extract()
+        yield {'title': title, 'author': author, 'tag': tag}
