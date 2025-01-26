968883b5e98072e316d6e380e1ef31210a68c26b
Santosh Kumar <sntshkmr60@gmail.com>
2025-01-26 05:55:36 +0530

    introducing xpath based data extraction

--- scrapy_intro/spiders/quote.py
+++ scrapy_intro/spiders/quote.py
@@ -18,3 +18,14 @@ class QuoteSpider(scrapy.Spider):
         # extract all authors
         authors = response.css('div.quote>span small.author::text').extract()
         yield {'authors': authors}
+
+        # another example using xpath
+        for quote in response.xpath('//div[@class="quote"]'):
+            text = quote.xpath('span[@class="text"]/text()').extract_first()
+            author = quote.xpath('span/small[@class="author"]/text()').extract_first()
+            yield {'text': text, 'author': author}
+
+        # combining css selectors and xpath
+        next_link = response.css("li.next a").xpath("@href").extract_first()
+        
+        yield {'next_link': next_link}
