5659be28a0966f1109997726866ea320293fe320
Santosh Kumar <sntshkmr60@gmail.com>
2025-01-28 13:48:53 +0530

    remove sqlite code

--- scrapy_intro/pipelines.py
+++ scrapy_intro/pipelines.py
@@ -3,8 +3,6 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 import logging
-import os
-import sqlite3
 
 # from dotenv import load_dotenv
 import pymongo
@@ -17,13 +15,7 @@ from itemadapter import ItemAdapter
 
 class ScrapyIntroPipeline:
     def __init__(self):
-        # self.create_sqlite_connection()
-        # self.create_sqlite_table()
         self.create_mongo_connection()
-
-    # def create_sqlite_connection(self):
-    #     self.conn = sqlite3.connect("quotes.db")
-    #     self.curr = self.conn.cursor()
 
     def create_mongo_connection(self):
         self.client = pymongo.MongoClient("mongodb://santosh:santosh@localhost:27017/")
@@ -40,12 +32,6 @@ class ScrapyIntroPipeline:
         logging.info("is mongo working?")
 
 
-    # def create_sqlite_table(self):
-    #     self.curr.execute("""create table if not exists quote(
-    #         title text,
-    #         author text,
-    #         tag text)""")
-
     def process_item(self, item, spider):
         # self.store_to_sqlite(item)
         self.store_to_mongo(item)
@@ -56,11 +42,3 @@ class ScrapyIntroPipeline:
     def store_to_mongo(self, item):
         self.collection.insert_one(dict(item))
 
-    # def store_to_sqlite(self, item):
-    #     self.curr.execute("""insert into quote values (?,?,?)""", (
-    #         item['title'],
-    #         item['author'],
-    #         ",".join(item['tag'])
-    #     ))
-
-    #     self.conn.commit()
