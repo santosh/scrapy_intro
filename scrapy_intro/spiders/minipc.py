b3825a7f8d39fdca8760598d5c4db6ed9cfe8d86
Santosh Kumar <sntshkmr60@gmail.com>
2025-01-29 23:59:53 +0530

    scrape minipc on amazon.com

--- scrapy_intro/spiders/minipc.py
+++ scrapy_intro/spiders/minipc.py
@@ -1,10 +1,28 @@
 import scrapy
 
+from ..items import MiniPC
 
 class MinipcSpider(scrapy.Spider):
     name = "minipc"
-    allowed_domains = ["amazon.in"]
-    start_urls = ["https://amazon.in"]
+    start_urls = ["https://www.amazon.com/s?k=dell+optiplex"]
 
     def parse(self, response):
-        pass
+        minipc = MiniPC()
+        search_results = response.css("div[role='listitem']")
+
+        for result in search_results:
+            title_recipe = result.css('div[data-cy="title-recipe"]')
+            title = title_recipe.css("h2 span::text").get()
+
+            relative_url = title_recipe.css("a.a-link-normal::attr(href)").get()
+            price = result.css(".a-price .a-offscreen::text").get()
+            rating = result.css("i[data-cy='reviews-ratings-slot'] span::text").get()
+
+            minipc["title"] = title
+            minipc["price"] = price
+            minipc["url"] = relative_url
+            minipc["rating"] = rating
+
+            yield minipc
+
+
